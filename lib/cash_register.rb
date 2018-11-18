8ac09c85066693194eb5565ffe538184937cbbcf
a lan <bluenowte97@gmail.com>
2018-11-18 02:33:25 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,11 +1,17 @@
 
 class CashRegister
- #cash_register = CashRegister.new
- #cash_register_with_discount = CashRegister.new(20)
   attr_accessor :total, :discount
+  #need to do setter and getter with optional arguments
   def initialize(total = 0, discount = 20)
     @total = total
     @discount = discount
   end
 
+  def add_item(title, price)
+  total+= price
+  end  
+
+
 end
+#cash_register = CashRegister.new
+#cash_register_with_discount = CashRegister.new(20)
