e4c293e61fb1151c3f39c459294ad9dc1864913c
a lan <bluenowte97@gmail.com>
2018-11-18 01:45:09 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,4 +1,8 @@
 
+def cashRegister
+ cash_register = CashRegister.new
+ cash_register_with_discount = CashRegister.new(20)
+end  
 def new
   def initialize(total = 0, employee discount = 20)
   end
